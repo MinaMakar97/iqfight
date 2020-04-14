f97392fee1d4e5168f00ac92fe3f0d90a3febdc5
Fabrizio <fabry_98@hotmail.it>
2020-04-14 23:31:40 +0200

    Inizio pagina base

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Gioco from "./Gioco.js";
 
 function App() {
-  return (
-    <div className="App">
-      <p> IQFight </p>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<div className="contenitore-principale">
+					<div className="cerchio" id="cerchio-tl"></div>
+					<div className="cerchio" id="cerchio-tr"></div>
+					<div className="cerchio" id="cerchio-bl"></div>
+					<div className="cerchio" id="cerchio-br"></div>
+					<div className="contenuto"></div>
+					<Switch>
+						<Route path="/room/:id" component={Gioco}></Route>
+						<Route path="/"></Route>
+					</Switch>
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
