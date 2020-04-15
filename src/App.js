4b97b327efdc5a1143319d86b9f0c695c2c2c2fc
Fabrizio <fabry_98@hotmail.it>
2020-04-15 12:24:07 +0200

    Creata struttura per il progetto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Gioco from "./Gioco.js";
+import Gioco from "./pages/Gioco.js";
 
 function App() {
 	return (
