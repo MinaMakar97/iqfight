586fbd458ececf3a8c44ec261dbd33fcfa51636a
mina <minamakar7@gmail.com>
2020-04-15 20:15:05 +0200

    inserito cartella components e aggiunto titleBox

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Gioco from "./pages/Gioco.js";
+import TitleBox from "./components/TitleBox.js"
 
 function App() {
+
 	return (
 		<Router>
 			<div className="container-fluid sfondo p-4 p-lg-5">
@@ -12,11 +14,13 @@ function App() {
 					<div className="cerchio rounded-circle m-3" id="cerchio-tr"></div>
 					<div className="cerchio rounded-circle m-3" id="cerchio-bl"></div>
 					<div className="cerchio rounded-circle m-3" id="cerchio-br"></div>
-					<div className="container-fluid contenuto p-3 p-md-4"></div>
-					<Switch>
-						<Route path="/room/:id" component={Gioco}></Route>
-						<Route path="/"></Route>
-					</Switch>
+					<div className="container-fluid contenuto p-3 p-md-4">
+					<TitleBox titolo="Login"></TitleBox>
+						{/* <Switch>
+							<Route path="/room/:id" component={Gioco}></Route>
+							<Route path="/"></Route>
+						</Switch> */}
+					</div>
 				</div>
 			</div>
 		</Router>
---
+++ src/components/TitleBox.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class TitleBox extends React.Component{
+    render(){
+        return <div>
+            {this.props.titolo}
+        </div>
+    }
+}
+
+export default TitleBox;
