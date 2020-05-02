843cf6d183d46a313349b377d79ded62a95780ab
Fabrizio <fabry_98@hotmail.it>
2020-05-02 20:26:33 +0200

    Aggiunta classifica e cambiata leggermente card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,22 @@
 import React from "react";
 import "./Card.css";
 
-class Card extends React.Component{
-    render(){
-        return <div className="card shadow mt-3 mt-lg-4">
-            {/* la prima colonna */}
-            <div className="dis-img">
-                <img src={this.props.imagine} className="img-card" alt=""/> 
-            </div>
+class Card extends React.Component {
+	render() {
+		return (
+			<div className={"card shadow mt-3 mt-lg-4 " + this.props.className} style={this.props.style}>
+				{/* la prima colonna */}
+				<div className="dis-img">
+					<img src={this.props.immagine} className="img-card" alt="" />
+				</div>
 
-            {/* la seconda colonna */}
-            <div className="dis-text">
-                <div className="font-weight-bold a">
-                    {this.props.nome}
-                </div>
-                <div className="text-muted a">
-                    {this.props.punteggio}
-                </div>
-            </div>
-
-
-        </div>
-    }
+				{/* la seconda colonna */}
+				<div className="dis-text">
+					<div className="font-weight-bold a">{this.props.nome}</div>
+					<div className="text-muted a">{this.props.punteggio}</div>
+				</div>
+			</div>
+		);
+	}
 }
 export default Card;
---
+++ src/pages/Classifica.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import logo from "../img/iqfight-logo.png";
+import "./Classifica.css";
+import Card from "../components/Card";
+import userImage from "../img/user.png";
+
+export default class Classifica extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			giocatori: [],
+		};
+	}
+
+	componentDidMount() {
+		let xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = (e) => {
+			if (e.target.readyState === 4 && e.target.status === 200) {
+				const json = JSON.parse(e.target.responseText);
+				if (json["successo"]) this.setState({ giocatori: json["giocatori"] });
+				else document.getElementById("status").textContent = "Si è verificato un errore con la connessione al database";
+			}
+		};
+		xhr.onerror = (e) => {
+			document.getElementById("status").textContent = "Si è verificato un errore con la connessione al server";
+		};
+		xhr.open("GET", "http://skylion.zapto.org/iqfight/classifica.php");
+		xhr.send();
+	}
+
+	creaGiocatori() {
+		const classiPodio = ["primo-classifica", "secondo-classifica", "terzo-classifica"];
+		let cardColonna = [];
+		const giocatori = this.state.giocatori;
+		for (let i = 0; i < giocatori.length; i++) {
+			cardColonna.push(
+				<Card
+					className={i < 3 ? classiPodio[i] : ""}
+					nome={giocatori[i].username}
+					punteggio={giocatori[i].punteggio.toLocaleString() + " punti"}
+					immagine={giocatori[i].avatar || userImage}
+					style={{ animationDelay: i * 0.25 + "s" }}
+					key={i}></Card>
+			);
+		}
+		return cardColonna;
+	}
+
+	render() {
+		return (
+			<div className="pagina-classifica w-100 h-100 d-flex flex-column">
+				<div className="row">
+					<div className="col-12 text-center mb-4">
+						<img src={logo} alt="Logo di IQFight" className="w-50"></img>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-12 text-center">
+						<p className="titolo"> Classifica </p>
+					</div>
+				</div>
+				{this.state.giocatori.length === 0 ? (
+					<div className="centra">
+						<p id="status" className="text-center"></p>
+					</div>
+				) : (
+					<div className="row contenuto-classifica flex-wrap centra">{this.creaGiocatori()}</div>
+				)}
+			</div>
+		);
+	}
+}
