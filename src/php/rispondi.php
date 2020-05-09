cd64f7a4c4b7ef12939dfa732d89ea30062b62d2
Fabrizio <fabry_98@hotmail.it>
2020-05-09 18:30:39 +0200

    Backend per rispondere e gestione dello stato

---
+++ src/php/rispondi.php
@@ -0,0 +1,56 @@
+<?php
+    include "utils.php";
+
+    $dbConn = connettiDB();
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = $_SESSION["username"];
+        $idStanza = $_SESSION["idStanza"];
+        $json = prendiJson();
+        controllaParametri($json, "risposta");
+        $rispostaClient = $json["risposta"];
+        if (!tempoScaduto($dbConn, $idStanza, $tempoDomanda)) {
+            if (rispostaCorretta($dbConn, $username, $rispostaClient, $idStanza)) {
+                aggiornaPunteggio($dbConn, $username, $idStanza, $tempoDomanda);
+            }
+            aggiornaGiocatori($dbConn, $idStanza);
+            echo json_encode(["successo" => true]);
+        } else {
+            echo json_encode(["successo" => false, "motivazione" => "Tempo scaduto"]);
+        }
+    }
+
+    disconnettiDB($dbConn);
+
+    function rispostaCorretta(mysqli $dbConn, $username, $rispostaClient, $idStanza)
+    {
+        $info = infoStanza($dbConn, $idStanza);
+        $domanda = prendiDomanda($dbConn, $idStanza, $info["numDomanda"]);
+        $rispCorretta = $domanda["rispCorretta"];
+
+        $rispCorrettaClient = $rispCorretta == $rispostaClient;
+        $query = $dbConn->prepare("UPDATE partecipa SET rispostaCorretta = ? WHERE username = ?");
+        $query->bind_param("is", $rispCorrettaClient, $username);
+        $query->execute();
+
+        return $rispCorrettaClient;
+    }
+
+    function aggiornaPunteggio(mysqli $dbConn, $username, $idStanza, $tempoDomanda)
+    {
+        $query = $dbConn->prepare("SELECT UNIX_TIMESTAMP(timestampDomanda) as timeDomanda FROM stanza WHERE stanza.id = ?");
+        $query->bind_param("i", $idStanza);
+        $query->execute();
+        $result = $query->get_result();
+        $result = $result->fetch_assoc();
+        $timeDomanda = $result["timeDomanda"];
+
+        $secImpiegati = time() - $timeDomanda;
+        $punteggio = ($tempoDomanda - $secImpiegati) * 10;
+
+        $query = $dbConn->prepare("UPDATE partecipa SET punteggio = punteggio + ? WHERE username = ?");
+        $query->bind_param("is", $punteggio, $username);
+        $query->execute();
+
+        return $punteggio;
+    }
---
+++ src/php/stato.php
@@ -0,0 +1,120 @@
+<?php
+    include "utils.php";
+
+    $dbConn = connettiDB();
+
+    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        $username = $_SESSION["username"];
+        $idStanza = $_SESSION["idStanza"];
+
+        aggiornaUltimaRichiesta($dbConn, $username);
+
+        if (getNumDomanda($dbConn, $idStanza) < $numDomande) {
+            if (!tempoScaduto($dbConn, $idStanza, $tempoDomanda)) {
+                $query = $dbConn->prepare("SELECT aggiornaGiocatori FROM partecipa WHERE username = ?");
+                $query->bind_param("s", $username);
+                $query->execute();
+                $result = $query->get_result();
+                $result = $result->fetch_assoc();
+                $aggiornaGiocatori = $result["aggiornaGiocatori"];
+                if ($aggiornaGiocatori == 1) {
+                    $giocatori = statoGiocatori($dbConn, $idStanza);
+                    $tuttiRisposto = true;
+                    foreach ($giocatori as $g) {
+                        if ($g["risposto"] == 0) {
+                            $tuttiRisposto = false;
+                            break;
+                        }
+                    }
+                    if ($tuttiRisposto) {
+                        // Tutti hanno risposto, manda risultati
+                        $query = $dbConn->prepare("UPDATE stanza SET domandaDaCambiare = 1 WHERE id = ?");
+                        $query->bind_param("i", $idStanza);
+                        $query->execute();
+
+                        echo json_encode(["successo" => true, "azione" => "risultati", "giocatori" => creaRisultati($dbConn, $idStanza)]);
+                    }
+                    else {
+                        // Un giocatore ha risposto oppure è entrato/uscito
+                        echo json_encode(["successo" => true, "azione" => "aggiorna", "giocatori" => $giocatori]);
+                    }
+                    $query = $dbConn->prepare("UPDATE partecipa SET aggiornaGiocatori = 0 WHERE username = ?");
+                    $query->bind_param("s", $username);
+                    $query->execute();
+                }
+                else {
+                    // Controlla se qualcuno si è disconnesso
+                    $query = $dbConn->prepare("SELECT username, UNIX_TIMESTAMP(ultimaRichiesta) as ultimaRichiesta FROM partecipaWHERE idStanza = ?");
+                    $query->bind_param("i", $idStanza);
+                    $query->execute();
+                    $giocatori = $query->get_result();
+                    $giocatori = $giocatori->fetch_all(MYSQLI_ASSOC);
+                    $tempo = time();
+                    foreach($giocatori as $giocatore){
+                        if ($tempo - $giocatore["ultimaRichiesta"] > 4) {
+                            eliminaGiocatoreDaStanza($dbConn, $giocatore["username"], $idStanza);
+                        }
+                    }
+                    echo json_encode(["successo" => true, "azione" => null]);
+                }
+            } else {
+                // Tempo scaduto, manda i risultati
+                $query = $dbConn->prepare("UPDATE stanza SET domandaDaCambiare = 1 WHERE id = ?");
+                $query->bind_param("i", $idStanza);
+                $query->execute();
+
+                echo json_encode(["successo" => true, "azione" => "risultati", "giocatori" => creaRisultati($dbConn, $idStanza)]);
+            }
+        } else {
+            echo json_encode(["successo" => true, "azione" => "finita", "giocatori" => classificaGioco($dbConn, $idStanza)]);
+        }
+    }
+
+    disconnettiDB($dbConn);
+
+    function aggiornaUltimaRichiesta(mysqli $dbConn, $username)
+    {
+        $query = $dbConn->prepare("UPDATE partecipa SET ultimoTempo = CURRENT_TIMESTAMP WHERE username = ?");
+        $query->bind_param("s", $username);
+        $query->execute();
+    }
+
+    function getNumDomanda(mysqli $dbConn, $idStanza)
+    {
+        $query = $dbConn->prepare("SELECT numDomanda FROM stanza WHERE id = ?");
+        $query->bind_param("i", $idStanza);
+        $query->execute();
+        $result = $query->get_result();
+        $result = $result->fetch_assoc();
+        return $result["numDomanda"];
+    }
+
+    function creaRisultati(mysqli $dbConn, $idStanza)
+    {
+        $query = $dbConn->prepare("SELECT username, punteggio, rispostaCorretta FROM partecipa WHERE idStanza = ?");
+        $query->bind_param("i", $idStanza);
+        $query->execute();
+        $result = $query->get_result();
+        $result = $result->fetch_all(MYSQLI_ASSOC);
+        return $result;
+    }
+
+    function classificaGioco(mysqli $dbConn, $idStanza)
+    {
+        $query = $dbConn->prepare("SELECT utente.username, punteggio, avatar FROM partecipa,utente WHERE partecipa.username = utente.username AND idStanza = ? ORDER BY punteggio DESC LIMIT 3");
+        $query->bind_param("i", $idStanza);
+        $query->execute();
+        $row = $query->get_result();
+        $giocatori = $row->fetch_all(MYSQLI_ASSOC);
+        return $giocatori;
+    }
+
+    function statoGiocatori(mysqli $dbConn, $idStanza) {
+        $query = $dbConn->prepare("SELECT username, CASE WHEN rispostaCorretta IS NULL THEN 0 ELSE 1 END as risposto FROM partecipa WHERE idStanza = ?");
+        $query->bind_param("i", $idStanza);
+        $query->execute();
+        $result = $query->get_result();
+        $result = $result->fetch_all(MYSQLI_ASSOC);
+        return $result;
+    }
+?>
