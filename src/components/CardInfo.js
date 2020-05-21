01847967417fb7ff875f3ed1828dd85137fc8c36
mina <minamakar7@gmail.com>
2020-05-21 20:53:38 +0200

    Cambiato card info in modo che riesce a prendere più cose in input

--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -1,15 +1,28 @@
 import React from "react";
-import "./CardInfo.css"
+import "./CardInfo.css";
 
-class CardInfo extends React.Component{
-    render(){
-        return <div className={"div-card " + this.props.className} style={this.props.style}>
-            <div className="row nome-sezione w-100"> {this.props.nomeSezione}</div>
-            <div className="row sezione"> {this.props.sezione} </div>
-        </div>
-
-        
-    }
+class CardInfo extends React.Component {
+	render() {
+		return (
+			<div className={"div-card " + this.props.className + " row mb-4"} style={this.props.style}>
+				<div className={this.props.html !== undefined ? "col-7 col-sm-9 p-0 h-100" : null}>
+					<div className="row nome-sezione"> {this.props.nomeSezione}</div>
+					<div className="row sezione" style={this.props.tronca !== undefined ? { textOverflow: "ellipsis !important" } : null}>
+						<input
+							className="w-100"
+							style={{ border: "unset" }}
+							id={this.props.id}
+							type={this.props.tipo}
+							defaultValue={this.props.sezione}
+							name={this.props.id}
+							onKeyPress={this.props.invia}
+							disabled={this.props.disabled}></input>
+					</div>
+				</div>
+				{this.props.html}
+			</div>
+		);
+	}
 }
 
 export default CardInfo;
