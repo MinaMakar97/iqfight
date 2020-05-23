e9cc448cb1193899e68da57d694ff01859bc8184
Fabrizio <fabry_98@hotmail.it>
2020-05-23 10:57:29 +0200

    Aggiunto popup per modificare immagine di profilo

---
+++ src/components/PopupImmagine.js
@@ -0,0 +1,264 @@
+import React, { Component } from "react";
+import "./PopupImmagine.css";
+import ImgUtente from "../img/user.png";
+
+export default class Popup extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			immagine: this.props.immagine,
+			zoom: 1,
+			top: 0,
+			left: 0,
+			prevDrag: { x: 0, y: 0 },
+			ruota: 0,
+			distanzaPinch: 0,
+		};
+
+		this.canvas = React.createRef();
+		this.cambiaImmagine = this.cambiaImmagine.bind(this);
+		this.gestisciImmagine = this.gestisciImmagine.bind(this);
+		this.gestisciZoom = this.gestisciZoom.bind(this);
+		this.gestisciSposta = this.gestisciSposta.bind(this);
+		this.gestisciMouseDown = this.gestisciMouseDown.bind(this);
+		this.gestisciMouseUp = this.gestisciMouseUp.bind(this);
+		this.ruotaDestra = this.ruotaDestra.bind(this);
+		this.ruotaSinistra = this.ruotaSinistra.bind(this);
+		this.salvaImmagine = this.salvaImmagine.bind(this);
+		this.eliminaImmagine = this.eliminaImmagine.bind(this);
+		this.ridimensiona = this.ridimensiona.bind(this);
+
+		this.image = new Image();
+		this.image.src = this.props.immagine || ImgUtente;
+	}
+
+	gestisciImmagine(e) {
+		const file = e.target.files[0];
+		if (file) this.cambiaImmagine(URL.createObjectURL(file));
+	}
+
+	cambiaImmagine(imgSrc) {
+		this.image.src = imgSrc;
+		this.setState({ immagine: imgSrc });
+	}
+
+	gestisciZoom(e) {
+		const deltaY = -e.deltaY;
+		this.setState((prevState) => {
+			let scale = prevState.zoom * 0.001;
+			let newZoom = prevState.zoom + deltaY * scale;
+			newZoom = Math.max(0, newZoom);
+			return { zoom: newZoom };
+		});
+	}
+
+	gestisciSposta(e) {
+		if (e.targetTouches) {
+			if (e.targetTouches.length === 1)
+				// Panning
+				e = e.targetTouches[0];
+			else {
+				// Zooming
+				const touch1 = e.targetTouches[0];
+				const touch2 = e.targetTouches[1];
+				const distanza = Math.abs(touch1.pageX - touch2.pageX);
+				let newZoom;
+				const max = Math.max(this.image.width, this.image.height);
+				const scale = 15;
+				if (distanza < this.state.distanzaPinch) newZoom = this.state.zoom - scale / max;
+				else newZoom = this.state.zoom + scale / max;
+				this.setState({ distanzaPinch: distanza, zoom: Math.max(0, newZoom) });
+				return;
+			}
+		}
+		const deltaX = e.pageX - this.state.prevDrag.x;
+		const deltaY = e.pageY - this.state.prevDrag.y;
+		this.setState((prevState) => ({
+			left: prevState.left + deltaX,
+			top: prevState.top + deltaY,
+			prevDrag: { x: e.pageX, y: e.pageY },
+		}));
+	}
+
+	gestisciMouseDown(e) {
+		e.preventDefault();
+		let pageX,
+			pageY,
+			move,
+			up,
+			distanza = 0;
+		// Desktop
+		if (e.pageX) {
+			pageX = e.pageX;
+			pageY = e.pageY;
+			move = "mousemove";
+			up = "mouseup";
+		}
+		// Mobile
+		else {
+			pageX = e.targetTouches[0].pageX;
+			pageY = e.targetTouches[0].pageY;
+			move = "touchmove";
+			up = "touchend";
+			if (e.targetTouches.length > 1) distanza = Math.abs(e.targetTouches[0].pageX - e.targetTouches[1].pageX);
+		}
+		this.setState({ prevDrag: { x: pageX, y: pageY }, distanzaPinch: distanza });
+		document.addEventListener(move, this.gestisciSposta);
+		document.addEventListener(up, this.gestisciMouseUp);
+	}
+
+	gestisciMouseUp(e) {
+		const move = e.targetTouches ? "touchmove" : "mousemove";
+		const up = e.targetTouches ? "touchend" : "mouseup";
+		document.removeEventListener(move, this.gestisciSposta);
+		document.removeEventListener(up, this.gestisciMouseUp);
+	}
+
+	ruotaDestra(e) {
+		this.setState((prevState) => ({ ruota: prevState.ruota + Math.PI / 2 }));
+	}
+
+	ruotaSinistra(e) {
+		this.setState((prevState) => ({ ruota: prevState.ruota - Math.PI / 2 }));
+	}
+
+	salvaImmagine() {
+		const canvas = this.canvas.current;
+		const ctx = canvas.getContext("2d");
+		const w = canvas.width;
+		const h = canvas.height;
+		let imgData;
+		if (w > h) {
+			imgData = ctx.getImageData(w / 2 - h / 2, 0, w / 2 + h / 2, h);
+		} else {
+			imgData = ctx.getImageData(0, h / 2 - w / 2, w, h / 2 + w / 2);
+		}
+
+		const newCanvas = document.createElement("canvas");
+		const min = Math.min(w, h);
+		newCanvas.width = min;
+		newCanvas.height = min;
+		const newCtx = newCanvas.getContext("2d");
+		newCtx.putImageData(imgData, 0, 0);
+		const newImg = newCanvas.toDataURL("image/png");
+
+		let formData = new FormData();
+		formData.append("avatar", newImg);
+
+		const xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = (e) => {
+			if (e.target.readyState === 4 && e.target.status === 200) {
+				let json = JSON.parse(e.target.responseText);
+				if (json["successo"] === true) {
+					if (this.props.salva) this.props.salva();
+				}
+			}
+		};
+		xhr.withCredentials = true;
+		xhr.open("POST", process.env.REACT_APP_LOCAL_ENDPOINT + "/iqfight/carica-immagine.php");
+		xhr.send(formData);
+	}
+
+	eliminaImmagine() {
+		this.image.src = ImgUtente;
+		this.setState({ immagine: ImgUtente });
+	}
+
+	disegnaGriglia(canvas) {
+		const ctx = canvas.getContext("2d");
+		const n = 30;
+		const lung = Math.ceil(Math.max(canvas.width, canvas.height) / n);
+		for (let y = 0; y < n; y++) {
+			for (let x = 0; x < n; x++) {
+				ctx.fillStyle = (x + y) % 2 === 0 ? "white" : "#ddd";
+				ctx.fillRect(x * lung, y * lung, lung, lung);
+			}
+		}
+	}
+
+	componentDidUpdate() {
+		const canvas = this.canvas.current;
+		const ctx = canvas.getContext("2d");
+		ctx.save();
+		this.disegnaGriglia(canvas);
+		ctx.translate(this.state.left, this.state.top);
+		ctx.scale(this.state.zoom, this.state.zoom);
+		ctx.rotate(this.state.ruota);
+		ctx.translate(-this.image.width / 2, -this.image.height / 2);
+		ctx.drawImage(this.image, 0, 0);
+		ctx.restore();
+	}
+
+	ridimensiona() {
+		const canvas = this.canvas.current;
+		canvas.width = canvas.parentElement.offsetWidth;
+		canvas.height = canvas.parentElement.offsetHeight;
+		const divCerchio = canvas.nextElementSibling;
+		const dimensioneMin = Math.min(canvas.width, canvas.height);
+		divCerchio.style.width = dimensioneMin + "px";
+		divCerchio.style.height = dimensioneMin + "px";
+		this.componentDidUpdate();
+	}
+
+	componentDidMount() {
+		const canvas = this.canvas.current;
+
+		canvas.addEventListener("wheel", this.gestisciZoom);
+		canvas.addEventListener("mousedown", this.gestisciMouseDown);
+		canvas.addEventListener("touchstart", this.gestisciMouseDown);
+
+		this.image.onload = () => {
+			const zoom = Math.max(canvas.height / this.image.height, canvas.width / this.image.width);
+			this.setState({
+				immagine: this.image.src,
+				zoom: zoom,
+				left: canvas.width / 2,
+				top: canvas.height / 2,
+			});
+		};
+
+		this.ridimensiona();
+		window.addEventListener("resize", this.ridimensiona);
+	}
+
+	componentWillUnmount() {
+		const canvas = this.canvas.current;
+		canvas.removeEventListener("wheel", this.gestisciZoom);
+		// togli listener mobile
+		canvas.removeEventListener("mousedown", this.gestisciMouseDown);
+		window.removeEventListener("resize", this.ridimensiona);
+		this.image.onload = null;
+	}
+
+	render() {
+		return (
+			<div className="iqfight-popup" onClick={this.props.annulla}>
+				<div className="iqfight-popup-bg" onClick={(e) => e.stopPropagation()}>
+					<div className="row div-immagine">
+						<canvas ref={this.canvas}></canvas>
+						<div className="cerchio-img"></div>
+					</div>
+					<div className="div-controlli row">
+						<i className="fas fa-undo" onClick={this.ruotaSinistra}></i>
+						<i className="fas fa-trash-alt" onClick={this.eliminaImmagine}></i>
+						<i className="fas fa-undo fa-flip-horizontal" onClick={this.ruotaDestra}></i>
+					</div>
+					<div className="row">
+						<label className="carica-immagine">
+							Carica immagine
+							<input type="file" accept="image/*" hidden onChange={this.gestisciImmagine}></input>
+						</label>
+					</div>
+					<div className="div-pulsanti row">
+						<button className="annulla" onClick={this.props.annulla}>
+							Annulla
+						</button>
+						<button className="salva" onClick={this.salvaImmagine}>
+							Salva
+						</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
--- src/php/carica-immagine.php
+++ src/php/carica-immagine.php
@@ -4,33 +4,50 @@
     $dbConn = connettiDB();
 
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        $username = isset($_SESSION["username"]) ? $_SESSION["username"] : $_SESSION["username_temp"];
+
+        // Crea la cartella degli avatars se non esiste
         $target_dir = "avatars/";
         if (!file_exists($target_dir)) {
             mkdir($target_dir);
         }
-        $source_file = $_FILES["avatar"]["name"];
-        $imageFileType = strtolower(pathinfo($source_file,PATHINFO_EXTENSION));
-        $target_file = $target_dir . basename($_SESSION["username_temp"] . "." . $imageFileType);
-        $uploadOk = 1;
 
-        // Check file size
-        if ($_FILES["avatar"]["size"] > 10000000) {
-            die(json_encode(["successo"=> false, "motivazione" => "Mi dispiace file troppo grande"]));
+        $target_file = null;
+        if (!isset($_FILES["avatar"])) {
+            // Immagine ritagliata dalla canvas
+            $imgData = base64_decode(explode(",", $_POST["avatar"])[1]);
+            $target_file = "avatars/" . $username . ".png";
+            $success = file_put_contents($target_file, $imgData);
+            if (!$success)
+                die(json_encode(["successo" => false, "motivazione" => "Impossibile caricare l'immagine"]));
+        } else {
+            // Immagine della registrazione
+            $source_file = $_FILES["avatar"]["name"];
+            $imageFileType = strtolower(pathinfo($source_file,PATHINFO_EXTENSION));
+            $target_file = $target_dir . basename($username . "." . $imageFileType);
+            $uploadOk = 1;
+    
+            // Check file size
+            if ($_FILES["avatar"]["size"] > 10000000) {
+                die(json_encode(["successo"=> false, "motivazione" => "Mi dispiace file troppo grande"]));
+            }
+            // Allow certain file formats
+            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+                die (json_encode(["successo"=> false, "motivazione" => "Formato del file errato"]));
+    
+            }
+            // if everything is ok, try to upload file
+            if (!move_uploaded_file($_FILES["avatar"]["tmp_name"], $target_file)) {
+                die(json_encode(["successo"=> false, "motivazione" => "c'e' stato un errore nel caricamento imagine"]));
+            }
         }
-        // Allow certain file formats
-        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-            die (json_encode(["successo"=> false, "motivazione" => "Formato del file errato"]));
+        
+        $query = $dbConn -> prepare("UPDATE utente SET avatar = ? WHERE username = ?");
+        $path = "/iqfight/" . $target_file;
+        $query->bind_param("ss", $path, $username);
+        $query->execute();
 
-        }
-        // if everything is ok, try to upload file
-        if (!move_uploaded_file($_FILES["avatar"]["tmp_name"], $target_file)) {
-            die(json_encode(["successo"=> false, "motivazione" => "c'e' stato un errore nel caricamento imagine"]));
-        }
         echo json_encode(["successo" => true]);
     }
-    $query = $dbConn -> prepare("UPDATE utente SET avatar = ? WHERE username = ?");
-    $path = "/iqfight/" . $target_file;
-    $query->bind_param("ss", $path, $_SESSION["username_temp"]);
-    $query->execute();
     disconnettiDB($dbConn);
 ?>
